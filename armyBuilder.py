66d571f84ff1415711a5ef8707da17cf4285633b
pntbttrvbs <justinspott@gmail.com>
2018-11-19 21:29:15 -0700

    subclassed widgets within the armyBuilder. dummy points progress bar implemented.
    I am going to try to flesh out some major UI details before I delve too far into the data structures for the armies.

--- armyBuilder.py
+++ armyBuilder.py
@@ -3,27 +3,46 @@ kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.uix.progressbar import ProgressBar
 
 class armyBuilder(App):
 
     def build(self):
-        topBar = AnchorLayout(anchor_x='center', anchor_y='top', size_hint=(1, 0.1))
-        pointsLayout = AnchorLayout(anchor_x='right', anchor_y='center')
+        return topBar()
+
+class topBar(BoxLayout):
+    def __init__(self, **kwargs):
+        #topBar = AnchorLayout(anchor_x='center', anchor_y='top', size_hint=(1, 0.1))
+        #original topBar above, I think by subclassing the boxlayout, I can just insert the desired modules.
+        super(topBar, self).__init__(**kwargs)
+        self.orientation = 'horizontal'
+        self.size_hint =(1,0.1)
+        #point this depending on faction choice
+        self.add_widget(Image(source = 'Images/ucmlogo.jpg'))
+        self.add_widget(pointsLayout())
+
+
+
+class pointsLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super(pointsLayout, self).__init__(**kwargs)
+        self.orientation = 'vertical'
         pts = 500
-        # this will reference the points sum of the current army.
+        maxPts = 1000
+        gameSize = 'CLASH'
+        # this will point to the points sum of the current army, as well as the selected game size.
+        pointsLabel = Label(text=gameSize + ': ' + str(pts) + '//' + str(maxPts) + ' POINTS', size_hint=(0.25,0.25))
+        pointsBar = ProgressBar(max = maxPts, size_hint=(0.25, 0.25))
+        pointsBar.value = pts
+        #let's think about putting a method into this class to update the value?
+        self.add_widget(pointsLabel)
+        self.add_widget(pointsBar)
 
-        lbl = Label(text=str(pts))
-        #        btn = Button(text='Press me', size_hint=(0.25,0.25))
 
-        pb = ProgressBar(max=1000, size_hint=(0.25, 0.25))
-        pb.value = pts
 
-        pointsLayout.add_widget(lbl)
-        #        topBar.add_widget(btn)
-        pointsLayout.add_widget(pb)
-        topBar.add_widget(pointsLayout)
-
-        return topBar
+if __name__ == '__main__':
+    armyBuilder().run()
